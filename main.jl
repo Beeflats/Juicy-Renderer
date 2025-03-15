f3f94aae8cb1f36ef33bf84d557b071a1c592dad
beeflats <z5468683@zmail.unsw.edu.au>
2025-03-15 15:43:28 +1100

    Monochrome render of a single object in front of the camera

--- main.jl
+++ main.jl
@@ -1,7 +1,10 @@
 using Images, ImageView
 
+# Set up the scene
+camera = makeCamera(PointVector(0, 0, 0), î + ĵ + k̂ )
+sphere = Sphere(PointVector(1, 1, 1), 0.2) # ball of 10cm radius sitting in front of the camera
+
 # Initialise image
-camera = makeCamera(PointVector(0, 0, 0), î + ĵ + k̂, 1)
 sensor = camera.sensor
 imageHeight = sensor.imageHeight
 imageWidth = sensor.imageWidth
@@ -10,7 +13,12 @@ image = zeros(3, imageHeight, imageWidth)
 # Render image
 for i ∈ 1:imageHeight, j ∈ 1:imageWidth
     ray = Ray(camera.position, unit(sensor.sensorsPositions[i, j] → camera.position))
-    pixelColor = [ray.direction.x, ray.direction.y, ray.direction.z]
+    backgroundColor = [ray.direction.x, ray.direction.y, ray.direction.z]
+    if ray ∩ sphere
+        pixelColor = [1, 0, 0]
+    else
+        pixelColor = backgroundColor
+    end
     image[:, i, j] = pixelColor
 end
 
